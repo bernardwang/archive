768ecec32158bc000a28233ec14e7fc92752150c
Bernard Wang <bernard@bernard.wang>
2019-01-22 23:11:09 -0500

    Update content

--- src/js/sites.js
+++ src/js/sites.js
@@ -4,7 +4,7 @@ const sites = [{
   url: "http://www.bernard.wang",
   date: "2017 - Present",
   tech: "Vue.js, Webpack, Pug, Tachyons",
-  detail: "I started this redesign with the goal to show off my technical skills and build the first iteration of my portfolio site. That semester at school I really enjoyed working in React, so I took the opportunity to learn something similar, Vue.js. I used the vue-loader starter which included Webpack, Pug, Tachyons, autoprefixer and a bunch of other dev goodies I had never used before. While designing the site, I relied heavily on a responsive grid system and bright colorful highlight colors.",
+  detail: "I started this redesign with the goal to show my technical skills and build the first iteration of my portfolio site. At the time I started building this, I was really enjoying working in React so I took the opportunity to learn something similar, Vue.js. I used the vue-loader starter which included Webpack, Pug, Tachyons, autoprefixer and a bunch of other dev goodies I had never used before. While designing the site, I relied heavily on a responsive grid system and bright colorful highlight colors.",
   ascii: `
 ╔═════════════════════════╗
 ║                         ║
@@ -27,7 +27,7 @@ const sites = [{
   url: "http://archive.bernard.wang/stars",
   date: "2015 - 2017",
   tech: "HTML, SCSS, Gulp.js, D3.js",
-  detail: "The initial version of this site was built at the 2015 Boilermake hackathon at Purdue University. The hackathon had issues with the internet from the very beginning, so rather than work on a real hackathon project I started messing with some D3.js examples, eventually creating a starry sky visual that I adapted into a simple website. Aside from the fact that D3 was entirely unnecessary for this visualization and that the site was not at all performant, I had a lot of fun playing with the dynamically generated star effect.",
+  detail: "The initial version of this site was built at the 2015 Boilermake hackathon at Purdue University. The hackathon had issues with the internet from the start of the event, so rather than work on a real hackathon project I messed with some D3.js examples eventually creating a starry sky visual that I adapted into a simple website. Aside from the fact that D3 is unnecessary for this kind of visualization and that the site was not at all performant, I had a lot of fun playing with the dynamically generated star effect.",
   ascii: `
 ╔═════════════════════════╗
 ║ ∙    ∙        ∙ ∙  ∙    ║
@@ -50,7 +50,7 @@ const sites = [{
   url: "http://archive.bernard.wang/paper",
   date: "2014",
   tech: "HTML, SCSS, JQuery",
-  detail: "I'm not sure exactly how I got the idea for this site, but it was both impractical and unnecessary. I wanted the text to be entirely print, so I went through the trouble of printing text on a notebook, taking pictures of the notebook, and then using those images as my site. Of course, I knew it would be impossible to update the text without retaking images, and it would be much much easier to just code a few lines of HTML, but this idea was far more novel and fun. I was just getting interested into web development but it seemed I didn't want to write a website with code.",
+  detail: "I'm not sure how I came up with the idea behind this site, but it was definitely impractical and a bit silly. I wanted the text to be entirely print, so I went through the trouble of printing text on a notebook, taking pictures, and then using those images as my site. Of course, I knew it would be impossible to update the text and it would be much easier to just write HTML like normal people, but this idea was far more novel and fun (pun intended).",
   ascii: `
 ╔═════════════════════════╗
 ║                         ║
@@ -73,7 +73,7 @@ const sites = [{
   url: "http://archive.bernard.wang/bubble",
   date: "2013",
   tech: "HTML, CSS, ALOT of JQuery",
-  detail: "This was the first website I had ever coded in my life and it is also so embarrassing that I was considering deleting the code forever. Instead, I did exact opposite and now everyone can see my amazing adventures with JQUERY.ANIMATE(). I don't have much to say about the ridiculous concept behind this site, but I will say I have certainly come a long way.",
+  detail: "This was the first website I had ever coded in my life and it is also very embarrassing. I was considering deleting the code forever, but instead I'm posting it here so everyone can see my adventures using JQUERY.ANIMATE(). I don't have much to say about the ridiculous concept behind this site, but I think I have certainly come a long way in my web development skills.",
   ascii: `
 ╔═════════════════════════╗
 ║                         ║
